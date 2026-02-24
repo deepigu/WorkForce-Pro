f3fb656841563a729829a6a459ee959b1f01410d
Deepika Karunakaran <99636325+deepigu@users.noreply.github.com>
2026-02-24 09:16:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // ─── MOCK DATA ────────────────────────────────────────────────────────────────
 const INITIAL_EMPLOYEES = [
